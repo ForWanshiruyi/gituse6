efc98354343d5c5cb8011da36776bb1d87af2d63
wanshiruyi <wanshiruyi.com>
2022-07-17 17:09:52 +0800

    fix-444

--- src/com/mystudy/Book.java
+++ src/com/mystudy/Book.java
@@ -8,6 +8,7 @@ public class Book {
     public static void main(String[] args) {
         System.out.println("222");
         System.out.println("111");
+        System.out.println("444");
         System.out.println("333");
     }
 }
