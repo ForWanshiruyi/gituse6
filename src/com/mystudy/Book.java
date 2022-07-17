ba0c099cd3295e3c413d2d92f848df8261ba4dd1
wanshiruyi <wanshiruyi.com>
2022-07-17 17:03:37 +0800

    hot-222-333

--- src/com/mystudy/Book.java
+++ src/com/mystudy/Book.java
@@ -6,6 +6,8 @@ package com.mystudy;
  */
 public class Book {
     public static void main(String[] args) {
+        System.out.println("222");
         System.out.println("111");
+        System.out.println("333");
     }
 }
