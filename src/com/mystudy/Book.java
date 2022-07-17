edc715e7d8da8fca1f014809b79039279ca05c0e
wanshiruyi <wanshiruyi.com>
2022-07-17 17:00:40 +0800

    111-co

---
+++ src/com/mystudy/Book.java
@@ -0,0 +1,11 @@
+package com.mystudy;
+
+/**
+ * @author :dingyb
+ * @date : 2022/7/17 16:58
+ */
+public class Book {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
